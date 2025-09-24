79b1eb357a183446f830207056c29fc78d4d1269
Veegie <jbalanuik@gmail.com>
2025-09-23 22:06:27 -0400

    Add build script. Separate database again. Much cleaner.

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cat ./js/db.js ./js/siivaguessr.js > bundle.js
+sed -i '1s/^/{\n/' bundle.js
+echo '}' >> bundle.js
+terser bundle.js --compress --mangle --output ./js/bundle.min.js
+cat comment ./js/bundle.min.js > ./js/siivaguessr.min.js
+rm bundle.js
+rm ./js/bundle.min.js
--- js/db.js
+++ js/db.js
@@ -1,9 +1,3 @@
-/**
- * Sneaking a little peek? Yes, I could have obfuscated this more, but if someone really 
- * wants to cheat at a 'dle, they're gonna do it; no amount of trickery can prevent that.
- * This saves me the trouble of setting up an entire backend.
- */
-
 const db = {
 	"MFFy3zH81es": {
 		"title": "Break Down - Cave Story",
@@ -97481,3 +97475,4 @@ const db = {
 		"artist": "MaxTrax"
 	}
 };
+
