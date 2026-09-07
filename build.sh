d265c0d61c8f0022ca41ea3b3d12f419275c4152
Veegie <jbalanuik@gmail.com>
2026-09-06 22:26:11 -0400

    Update build script and gitignore.

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+mkdir -p dist/js
+cp -r img/ dist/img
+cp -r svg/ dist/svg
+cp index.html dist/index.html
+cp style.css dist/style.css
 cd js
 cat db.js siivaguessr.js > bundle.js
 sed -i '1s/^/{\n/' bundle.js
 echo '}' >> bundle.js
 terser confettea.js --compress --mangle --output confettea.min.js
-cat confettea-license confettea.min.js > temp.js && mv temp.js confettea.min.js
+cat confettea-license.js confettea.min.js > ../dist/js/confettea.min.js
 terser bundle.js --compress --mangle --output siivaguessr.min.js
-cat comment.js siivaguessr.min.js > temp.js && mv temp.js siivaguessr.min.js
+cat comment.js siivaguessr.min.js > ../dist/js/siivaguessr.min.js
 rm bundle.js
+rm confettea.min.js
+rm siivaguessr.min.js
